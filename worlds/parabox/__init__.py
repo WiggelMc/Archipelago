395d549ca6dc74730051be8fbe41a1fdfb28cb88
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-01-10 22:01:36 +0100

    Add more World Properties

--- worlds/parabox/__init__.py
+++ worlds/parabox/__init__.py
@@ -1,26 +1,28 @@
 from worlds.AutoWorld import World, WebWorld
 from worlds.parabox.options import ParaboxOptions
+from worlds.parabox.presets import option_presets
+from worlds.parabox.groups import item_name_groups
 
 PARABOX_GAME = "Patrick's Parabox"
 
 
 class ParaboxWebWorld(WebWorld):
     theme = "ocean"
+    options_presets = option_presets
 
 
 class ParaboxWorld(World):
     """
-    Patrick's Parabox is an award-winning puzzle game
+    Patrick's Parabox is a puzzle game
     that explores a unique recursive system
     of boxes within boxes within boxes within boxes.
-    Learn to manipulate the world's structure
-    by pushing boxes into and out of each other.
-    Wrap your head around what happens when a box contains itself,
-    and learn to use infinity to your advantage.
-    Explore many more mechanics and recursive twists
-    as you delve deeper and deeper into the system.
-    It's boxes all the way down.
     """
     game = PARABOX_GAME
     options_dataclass = ParaboxOptions
     options: ParaboxOptions
+    web = ParaboxWebWorld
+    topology_present = False
+
+    item_name_to_id = {}
+    location_name_to_id = {}
+    item_name_groups = item_name_groups
---
+++ worlds/parabox/presets.py
@@ -0,0 +1,7 @@
+from worlds.parabox.opt import shuffle_options
+
+option_presets = {
+    "Pain": {
+        "shuffle_undo": shuffle_options.ShuffleUndo.option_progressive
+    }
+}
