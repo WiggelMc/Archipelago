7c6ee24a93946310e9ce5804648679610f61e9d3
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-01-12 19:30:44 +0100

    Add Item Data Creators

--- worlds/parabox/items.py
+++ worlds/parabox/items.py
@@ -1,6 +1,37 @@
+from typing import NamedTuple
+
 from BaseClasses import Item
 from worlds.parabox import PARABOX_GAME
 
 
 class ParaboxItem(Item):
     game: str = PARABOX_GAME
+
+
+class ParaboxItemData(NamedTuple):
+    id: int
+    type: str
+
+
+class ParaboxItemDataCreator:
+    def __init__(self):
+        pass
+
+    def get_single(self) -> dict[str, ParaboxItem]:
+        return {}
+
+
+class ParaboxProgressiveItemDataCreator(ParaboxItemDataCreator):
+    def __init__(self):
+        super().__init__()
+
+    def get_progressive(self) -> dict[str, ParaboxItem]:
+        return {}
+
+
+class ParaboxProgressiveOrSeperateItemDataCreator(ParaboxProgressiveItemDataCreator):
+    def __init__(self):
+        super().__init__()
+
+    def get_seperate(self) -> dict[str, ParaboxItem]:
+        return {}
