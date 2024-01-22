cd832563f300540ae3c56fb4ef8e26cf60aec23c
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-01-22 21:00:58 +0100

    Add Items for Preset Blocks

--- worlds/parabox/external/item_info.py
+++ worlds/parabox/external/item_info.py
@@ -33,16 +33,22 @@ class ParaboxItemInfoDefinitions:
 
     recursion = ParaboxItemInfo("Recursion", 11)
     progressive_recursion = ParaboxItemInfo("Progressive Recursion", 12)
+
     flip = ParaboxItemInfo("Flip", 13)
     progressive_flip = ParaboxItemInfo("Progressive Flip", 14)
+
     friend = ParaboxItemInfo("Friend", 15)
     progressive_friend = ParaboxItemInfo("Progressive Friend", 16)
+
     infinite_exit_block = ParaboxItemInfo("Infinite Exit", 17)
     progressive_infinite_exit_block = ParaboxItemInfo("Progressive Infinite Exit", 18)
+
     infinite_enter_block = ParaboxItemInfo("Infinite Enter", 19)
     progressive_infinite_enter_block = ParaboxItemInfo("Progressive Infinite Enter", 20)
+
     player = ParaboxItemInfo("Player", 21)
     progressive_player = ParaboxItemInfo("Progressive Player", 22)
+
     undo = ParaboxItemInfo("Undo", 23)
     progressive_undo = ParaboxItemInfo("Progressive Undo", 24)
 
@@ -51,9 +57,10 @@ class ParaboxItemInfoDefinitions:
     possess_wall = ParaboxItemInfo("Possess Wall", 27)
     possess_box = ParaboxItemInfo("Possess Box", 28)
 
-    # TODO: Goal In Box
-    # TODO: Box Sizes
-    # TODO: Preset Boxes
+    nested_button = ParaboxItemInfo("Nested Button", 29)
+    progressive_nested_button = ParaboxItemInfo("Progressive Nested Button", 30)
+    nested_box_button = ParaboxItemInfo("Nested Box Button", 31)
+    nested_player_button = ParaboxItemInfo("Nested Player Button", 32)
 
     slowness_trap = ParaboxItemInfo("Slowness Trap", 40, ParaboxItemType.TRAP)
     zoom_in_trap = ParaboxItemInfo("Zoom In Trap", 41, ParaboxItemType.TRAP)
@@ -68,6 +75,41 @@ class ParaboxItemInfoDefinitions:
     open_puzzle_help = ParaboxItemInfo("Open Puzzle Help", 49, ParaboxItemType.USEFUL)
     center_puzzle_help = ParaboxItemInfo("Center Puzzle Help", 50, ParaboxItemType.USEFUL)
     exit_puzzle_help = ParaboxItemInfo("Exit Puzzle Help", 51, ParaboxItemType.USEFUL)
+
+    # EXPERIMENTAL #
+    l_full_box_3 = ParaboxItemInfo("L Full Box (3)", 60)
+    t_full_box_3 = ParaboxItemInfo("T Full Box (3)", 61)
+    i_full_box_3 = ParaboxItemInfo("I Full Box (3)", 62)
+
+    pi_full_box_3 = ParaboxItemInfo("PI Full Box (3)", 63)
+    bi_full_box_3 = ParaboxItemInfo("BI Full Box (3)", 64)
+    bi_b_full_box_3 = ParaboxItemInfo("BI B Full Box (3)", 65)
+
+    d_full_box_3 = ParaboxItemInfo(". Full Box (3)", 66)
+    bd_full_box_3 = ParaboxItemInfo("B. Full Box (3)", 67)
+
+    empty_box_3 = ParaboxItemInfo("Empty Box (3)", 68)
+    b_empty_box_3 = ParaboxItemInfo("B Empty Box (3)", 69)
+    p_empty_box_3 = ParaboxItemInfo("P Empty Box (3)", 70)
+    w_empty_box_3 = ParaboxItemInfo("W Empty Box (3)", 71)
+
+    l_full_box_5 = ParaboxItemInfo("L Full Box (5)", 72)
+    i_full_box_5 = ParaboxItemInfo("I Full Box (5)", 73)
+
+    empty_box_5 = ParaboxItemInfo("Empty Box (5)", 74)
+    b_empty_box_5 = ParaboxItemInfo("B Empty Box (5)", 75)
+    p_empty_box_5 = ParaboxItemInfo("P Empty Box (5)", 76)
+    w_empty_box_5 = ParaboxItemInfo("W Empty Box (5)", 77)
+
+    bj_b_full_box_5 = ParaboxItemInfo("BJ B Full Box (5)", 78)
+    bol_full_box_5 = ParaboxItemInfo("BOL Full Box (5)", 79)
+    d_hollow_box_5 = ParaboxItemInfo(". Hollow Box (5)", 80)
+    dd_hollow_box_5 = ParaboxItemInfo(".. Hollow Box (5)", 81)
+    open_hollow_box_5 = ParaboxItemInfo("Open Hollow Box (5)", 82)
+    line_empty_box_5 = ParaboxItemInfo("Line Empty Box (5)", 83)
+    bbi_full_box_5 = ParaboxItemInfo("BBI Full Box (5)", 84)
+    bxi_full_box_5 = ParaboxItemInfo("BXI Full Box (5)", 85)
+    # EXPERIMENTAL #
 
 
 class ParaboxItemGenerator:
@@ -178,6 +220,7 @@ class LevelSelectItemGenerator(ParaboxItemGenerator):
 class ParaboxItemInfoGeneratorDefinitions:
     keys = KeyItemGenerator(100)
     level_selects = LevelSelectItemGenerator(200)
+    # TODO: Box Sizes
 
 
 def get_item_name_to_id():
